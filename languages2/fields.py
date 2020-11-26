244a0e3528cbbd27e1edacb6a6363808532b9123
Jirka Schaefer <jirka@tschitschereengreen.com>
2020-11-26 14:39:49 +0100

    package code

--- languages2/fields.py
+++ languages2/fields.py
@@ -1,13 +1,15 @@
 from django.db.models.fields import CharField
 
+
 class LanguageField(CharField):
     """
     A language field for Django models.
     """
+
     def __init__(self, *args, **kwargs):
         # Local import so the languages aren't loaded unless they are needed.
         from django_languages.languages import LANGUAGES
 
-        kwargs.setdefault('max_length', 3)
-        kwargs.setdefault('choices', LANGUAGES)
+        kwargs.setdefault("max_length", 8)
+        kwargs.setdefault("choices", LANGUAGES)
         super(CharField, self).__init__(*args, **kwargs)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Languages2",
+    version="0.0.1",
+    author="Patroqueeet",
+    author_email="jirka@tschitschereengreen.com",
+    packages=["languages2"],
+    description="forked unmainted repo",
+    long_description=open("README.txt").read(),
+    install_requires=["Django >= 3.0.1",],
+)
