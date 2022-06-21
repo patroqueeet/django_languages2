e1abf44322c1f56d46b23bdfc8caccdfa355f248
Jirka Schaefer <jirka@tschitschereengreen.com>
2022-06-21 10:58:08 +0200

    bump 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="Languages2",
-    version="0.0.5",
+    version="0.0.6",
     author="Patroqueeet",
     author_email="jirka@tschitschereengreen.com",
     packages=["languages2"],
